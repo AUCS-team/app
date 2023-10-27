d7f82139cb3d9717156fbcd7b302a0194d76aeb9
cialloo <aluckywenda@gmail.com>
2023-10-27 11:51:32 +0000

    remove mongo module, merge it to main app

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,26 @@
+const { MongoClient } = require('mongodb');
+const config = require('./config');
+
+const mongoUrl = 'mongodb://' + config.database.host + ':' + config.database.port;
+const mongoClient = new MongoClient(mongoUrl);
+const mongoDatabaseName = config.database.name;
+const userCollectionName = 'user';
+const videoCollectionName = 'video';
+
+let mongoConnection = null;
+let mongoDatabase = null;
+let mongoUserCollection = null;
+let mongoVideoCollection = null;
+
+async function initMongoDatabaseConnection() {
+    mongoConnection = await mongoClient.connect();
+    mongoDatabase = mongoConnection.db(mongoDatabaseName);
+    mongoUserCollection = mongoDatabase.collection(userCollectionName);
+    mongoVideoCollection = mongoDatabase.collection(videoCollectionName);
+}
+
 async function main() {
+    await initMongoDatabaseConnection();
     console.log('hello world');
 }
 
