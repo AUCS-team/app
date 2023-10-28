c9fbe98dfc3fb14a82e3110764b02455b9d44148
cialloo <aluckywenda@gmail.com>
2023-10-28 01:07:35 +0000

    add init grpc server

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const { MongoClient } = require('mongodb');
 const config = require('./config');
+const grpc = require('@grpc/grpc-js');
+const grpcProtoLoader = require('@grpc/proto-loader');
 
 const mongoUrl = 'mongodb://' + config.database.host + ':' + config.database.port;
 const mongoClient = new MongoClient(mongoUrl);
 const mongoDatabaseName = config.database.name;
 const userCollectionName = 'user';
 const videoCollectionName = 'video';
+
+const grpcUrl = '0.0.0.0:8080';
 
 let mongoConnection = null;
 let mongoDatabase = null;
@@ -19,8 +23,25 @@ async function initMongoDatabaseConnection() {
     mongoVideoCollection = mongoDatabase.collection(videoCollectionName);
 }
 
+function initGrpcServer() {
+    let server = new grpc.Server();
+    let packageDefinition = grpcProtoLoader.loadSync('../share/protobuf/app.proto');
+    let appProto = grpc.loadPackageDefinition(packageDefinition).app;
+    server.addService(appProto.Meta.service, {
+        echo: echo,
+    });
+    server.bindAsync(grpcUrl, grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+    });
+}
+
+function echo(call, callback) {
+    callback(null, { "content": "This is Meta service, here is your message:" + call.request.content });
+}
+
 async function main() {
     await initMongoDatabaseConnection();
+    initGrpcServer();
     console.log('hello world');
 }
 
